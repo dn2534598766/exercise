fd8df74239fa1e714c4021212a4ac9ccac259b9f
dn2534598766 <2534598766@qq.com>
2021-02-02 22:11:53 +0800

    commit

---
+++ vue.js/js/Es6模块化导出导入/b.js
@@ -0,0 +1,8 @@
+import {a} from './a.js'
+console.log(a)
+import {b,name,sex} from './a.js'
+console.log(b,name,sex)
+import humour from './a.js'
+console.log(humour(10,20))
+import * as aaa from './a.js'
+console.log(aaa.sum(2,3))
