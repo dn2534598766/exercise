fdfe91241eeaf77da636211cf86013f5774c9fb5
dn2534598766 <2534598766@qq.com>
2021-02-08 20:16:25 +0800

    commit

---
+++ vue.js/webpack中plugin/src/vue/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2 class="title">Talk is cheap,show me the code.</h2>
+    <p>{{message}}</p>
+    <Cpn></Cpn>
+  </div> 
+</template>
+
+<script>
+  import Cpn from './Cpn.vue'
+  export default {
+    name:'App',
+    data(){
+      return {
+        message:'我是vue实例,哈哈哈'
+      }
+    },
+    components:{
+      Cpn
+    }
+  }
+</script>
+
+<style scoped>
+.title{
+  color:green
+}
+</style>
