3f2dac9ffd4287cdaaa1d1826564e7fbb55112a2
dn2534598766 <2534598766@qq.com>
2021-02-22 16:02:53 +0800

    commit

--- vue.js/vuecli/tabbar/src/App.vue
+++ vue.js/vuecli/tabbar/src/App.vue
@@ -1,38 +1,16 @@
 <template>
   <div id="app">
-    <tab-bar>
-      <tab-bar-item>
-        <img src="./assets/img/home.svg" slot="img"/>
-        <img src="./assets/img/home_active.svg" slot="img-active">
-        <div slot="text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img src="./assets/img/sort.svg" slot="img"/>
-        <img src="./assets/img/sort_active.svg" slot="img-active">
-        <div slot="text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img src="./assets/img/cart.svg" slot="img"/>
-        <img src="./assets/img/cart_active.svg" slot="img-active">
-        <div slot="text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img src="./assets/img/profile.svg" slot="img"/>
-        <img src="./assets/img/profile_active.svg" slot="img-active">
-        <div slot="text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <router-view></router-view>
+      <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from './views/TabBar/TabBar'
-import TabBarItem from './views/TabBar/TabBarItem'
+import MainTabBar from 'components/mainTabBar/MainTabBar.vue'
 export default {
   name: 'App',
   components:{
-    TabBar,
-    TabBarItem
+   MainTabBar
   }
 }
 </script>
--- vue.js/vuecli/tabbar/src/main.js
+++ vue.js/vuecli/tabbar/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  render: h => h(App),
+  
 })
