6eba3ceb677e2664c091b2682d1096ccc3535c5a
dn2534598766 <2534598766@qq.com>
2021-03-03 20:02:53 +0800

    commit

--- vue.js/vuecli/learnaxios/src/router/index.js
+++ vue.js/vuecli/learnaxios/src/router/index.js
@@ -12,14 +12,30 @@ const router = new Router({
 })
 export default router
 
-axios({
-  url:'http://123.207.32.32:8000/home/multidata'
-}).then(res=>console.log(res))
+// axios({
+//   url:'http://123.207.32.32:8000/home/multidata'
+// }).then(res=>console.log(res))
 
-axios({
-  url:'http://123.207.32.32:8000/home/data',
-  params:{
-    type:'pop',
-    page:1
-  }
-}).then(res=>console.log(res))
+// axios({
+//   url:'http://123.207.32.32:8000/home/data',
+//   params:{
+//     type:'pop',
+//     page:1
+//   }
+// }).then(res=>console.log(res))
+axios.defaults.baseURL='http://123.207.32.32:8000'
+axios.defaults.timeout=5000
+axios.all([
+  axios({
+    url:'/home/multidata'
+  }),
+  axios({
+    url:'/home/data',
+    params:{
+      type:"sell",
+      page:3
+    }
+  })]).then(axios.spread((res1,res2)=>{
+    console.log(res1)
+    console.log(res2)
+  }))
