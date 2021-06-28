db6968536179e33e1fb40bc506baa8e1a1d81177
dn2534598766 <2534598766@qq.com>
2021-06-28 17:11:26 +0800

    commit

--- node.js/export.js
+++ node.js/export.js
@@ -1,5 +1,15 @@
-let obj = require('./index')
+// let obj = require('./index')
 
-obj.add(5,2)
-obj.sub(5,2)
-console.log(a)
+// obj.add(5,2)
+// obj.sub(5,2)
+// console.log(a)
+
+// let a = Buffer.allocUnsafe(20)
+
+// console.log(a.fill('eric',5))
+const a = Buffer.from('test')
+const b = Buffer.from('djaid')
+console.log(a.toString())
+console.log(a.toJSON())
+a.copy(b)
+console.log(b.toString())
