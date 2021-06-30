890baef4a9963f57130bb8f06a152cc7a8f30c76
dn2534598766 <2534598766@qq.com>
2021-06-30 21:49:06 +0800

    commit

--- node.js/export.js
+++ node.js/export.js
@@ -7,9 +7,42 @@
 // let a = Buffer.allocUnsafe(20)
 
 // console.log(a.fill('eric',5))
-const a = Buffer.from('test')
-const b = Buffer.from('djaid')
-console.log(a.toString())
-console.log(a.toJSON())
-a.copy(b)
-console.log(b.toString())
+// const a = Buffer.from('test')
+// const b = Buffer.from('djaid')
+// console.log(a.toString())
+// console.log(a.toJSON())
+// a.copy(b)
+// console.log(b.toString())
+const fs = require('fs')
+// fs.readFile('a.txt',(err,data)=>{
+//     console.log(data.toString())
+// })
+// fs.writeFile('a.txt','this is a test',err=>{
+//     if(err) throw err
+//     console.log('写入成功!')
+// })
+
+// const buf = Buffer.from('hello world!')
+// fs.appendFile('a.txt',buf,err=>{
+//     if(err) throw err
+//     console.log('追加成功!')
+// })
+
+// fs.stat('a.txt',(err,stats)=>{
+//     if(err) throw err 
+//     console.log(stats)
+//     console.log(stats.isFile())
+//     console.log(stats.isDirectory()) 
+// })
+
+// fs.rename('a.txt','hello.txt',err=>{
+//     if(err){
+//         throw err
+//     }
+//     console.log('改名成功!')
+// })
+
+fs.unlink('hello.txt',err=>{
+    if(err) throw err
+    console.log('删除文件成功!')
+})
