f6660ae5f0debf7c57917bc7e3ad4d309fa2824c
dn2534598766 <2534598766@qq.com>
2021-07-04 20:18:02 +0800

    commit

---
+++ node.js/file_test.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+// fs.mkdir('./a/b',{recursive:true},err=>{
+//     if(err) throw err
+//     console.log('创建文件夹成功!')
+// })
+
+
+// fs.readdir('./',{withFileTypes:true},(err,file)=>{
+//     if(err) throw err
+//     console.log(file)
+// })
+
+fs.rmdir('./a',{recursive:true},err=>{
+    if(err) throw err
+    console.log('删除文件夹成功!')
+})
